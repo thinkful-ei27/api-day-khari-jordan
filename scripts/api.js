e8ff7d7cd5fd2c333fd21060a5713695ef1932f5
Jordan Heffernan <jheff2@gmail.com>
2018-12-12 09:12:07 -0800

    added createItem function to POST new item to items

--- scripts/api.js
+++ scripts/api.js
@@ -3,13 +3,34 @@
 
 const api = (function () {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/khari-jordan';
+  
+  //returns data from GET request to items
   const getItems = function (callback) {
     callback('api module works!');
     $.getJSON(`${BASE_URL}/items`, callback);
   };
+  
+
+  //Sends POST request to items
+  const createItem = function (name, callback) {
+    console.log('createItem ran')
+    const newItem = JSON.stringify({
+        name: name,
+    });
+    $.ajax({
+      'url': `${BASE_URL}/items`,
+      'method': 'POST',
+      'contentType': 'application/json',
+      'data': newItem,
+      'success': callback
+    });
+  };
+
   return {
     BASE_URL,
     getItems,
+    createItem,
   };
 	
 }());
+
